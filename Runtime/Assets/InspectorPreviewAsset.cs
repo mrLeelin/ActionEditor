aa655644878872cc78a3900d3433760a9eb928c3
Lin <li18202213260@gmail.com>
2026-03-29 23:55:06 +0800

    Add editor preview undo snapshot state

--- Runtime/Assets/InspectorPreviewAsset.cs
+++ Runtime/Assets/InspectorPreviewAsset.cs
@@ -4,6 +4,12 @@ namespace NBC.ActionEditor
 {
     public class InspectorPreviewAsset : ScriptableObject
     {
-        
+        [HideInInspector] [SerializeField] private string serializedState;
+
+        public string SerializedState
+        {
+            get => serializedState;
+            set => serializedState = value;
+        }
     }
 }
